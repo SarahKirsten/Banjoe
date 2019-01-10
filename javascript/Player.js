c84472644ebbe3e258e2e102ae49e4696b527f7b
Tuff J Henson <thenson20@my.whitworth.edu>
2019-01-10 11:48:07 -0800

    Working on jQuery stuff

--- javascript/Player.js
+++ javascript/Player.js
@@ -4,6 +4,17 @@ var Banjoe = {
     direction : true,
     x_position : 0,
     y_position : 0,
-
+    speed : -10,
+    gifs : ["images/BanjoGuyRunning.gif", ],
+    $images : null,
  
+    init : function(){
+        this.$images = $.preload(this.gifs);
+        $("div#Banjoe").empty().append(this.$images[0]);
+    }
+
 }
+
+$(document).ready(function () {
+    Banjoe.init();
+});
